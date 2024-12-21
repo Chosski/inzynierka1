88a8bd84a76a2d75a4152864760cb5e8050bb0f2
Chosski <132196621+Chosski@users.noreply.github.com>
2024-12-21 21:17:27 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,10 +11,11 @@ const PORT = 3000;
 
 const dbConfig = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  dialect: "mysql",
 };
 
 const logger = winston.createLogger({
